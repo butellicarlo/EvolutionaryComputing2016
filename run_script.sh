5a47ce5b5a4306d3cf75472436861bbbe96142e8
Carlo <carlo@digifit.eu>
2016-10-09 15:01:57 +0200

    Run java commands right version

--- run_script.sh
+++ run_script.sh
@@ -2,15 +2,21 @@
 javac -cp contest.jar player9.java Individual.java Population.java
 jar cmf MainClass.txt submission.jar player9.class Individual.class Population.class
 
-if [ $1 -eq 1 ] ; then
-	echo "Sphere Evaluation"
-	java -jar testrun.jar -submission=player9 -evaluation=SphereEvaluation -seed=1
-else if [ $1 -eq 2 ] ; then
-	echo "Rastrigin Evaluation"
-	java -jar testrun.jar -submission=player9 -evaluation=RastriginEvaluation -seed=1
-else if [ $1 -eq 3 ] ; then
-	echo "Fletcher Powell Evaluation"
-	java -jar testrun.jar -submission=player9 -evaluation=FletcherPowellEvaluation -seed=1
-fi
-fi
-fi
+case "$1" in
+        1)
+            echo "Sphere Evaluation"
+			java -jar testrun.jar -submission=player9 -evaluation=SphereEvaluation -seed=1
+            ;;
+         
+        2)
+            echo "Rastrigin Evaluation"
+			java -jar testrun.jar -submission=player9 -evaluation=RastriginEvaluation -seed=1
+         	;;
+        3)
+            echo "Fletcher Powell Evaluation"
+			java -jar testrun.jar -submission=player9 -evaluation=FletcherPowellEvaluation -seed=1
+        	;;         
+        *)
+            echo $"Usage: $0 {1 for Sphere|2 for Rastrigin|3 for Fletcher}"
+            exit 1
+esac
